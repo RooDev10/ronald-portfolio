e81c9c29e69c11a7db913dc0d47cc32ecd58273d
Ronni-FSTK <u202224619@upc.edu.pe>
2026-01-07 11:54:18 -0500

    Completed projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,46 @@
 import styles from './Projects.module.css';
 import projects from '../../data/projects.js';
+import { useEffect, useRef } from 'react';
 
 function Projects() {
+
+    const cardsRef = useRef([]);
+
+    useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(styles.visible);
+          } else {
+            entry.target.classList.remove(styles.visible);
+          }
+        });
+      },
+      {
+        threshold: 0.3
+      }
+    );
+
+    cardsRef.current.forEach((card) => {
+      if (card) observer.observe(card);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
         <section id="Projects" className={styles.projectsSection}>
             <h1>Mis proyectos</h1>
-            <p className={styles.projectsDescription}>Una colección de mis trabajos recientes en desarrollo de software, diseño web y soluciones creativas.</p>
+            <p className={styles.projectsDescription}>Una colección de mis trabajos 
+                recientes en desarrollo de software, diseño web y soluciones creativas.</p>
 
             <div className={styles.projectsWrapper}>
                 <div className={styles.projectsContainer}>
                 {projects.map((project, index) => (
                     <article 
                         key={project.id_project} 
+                        ref={(el) => (cardsRef.current[index] = el)}
                         className={`${styles.projectCard} ${index % 2 !== 0 ? styles.reverse : ''}`}
                     >
                         {/* Bloque 1: Imagen */}
